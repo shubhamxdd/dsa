3e6c49d0877b3fa584dcd9769b3ab5aa1f84ac51
shubhamxdd <ssisodia128@gmail.com>
2025-10-27 23:09:33 +0530

    using binary search optimal

--- binary search/Binary search on answers/1square_root_logn.cpp
+++ binary search/Binary search on answers/1square_root_logn.cpp
@@ -14,7 +14,30 @@ int square_root(int num){
     return ans;
 }
 
+int square_root_binary_search(int num){
+    int low = 1;
+    int high = num - 1;
+    int ans = 1;
+
+    while(high>=low){
+        int mid = (low+high)/2;
+
+        if(mid*mid<=num){
+            // update ans and eliminate right
+            ans = mid;
+            // eliminate left
+            low = mid + 1;
+        }else {
+            high = mid - 1;
+        }
+    }
+    // to save space return high instead of using additional variable, as high will point to answer in last iteration
+    cout << "from function, high => " << high << endl;
+    return ans;
+}
+
 int main(){
-    cout << square_root(15);
+    // cout << square_root(15);
+    cout << square_root_binary_search(15) << endl;;
     return 0;
 }
