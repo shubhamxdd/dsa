aed379d93182e262e529a775e5488b922a2f2e26
shubhamxdd <ssisodia128@gmail.com>
2025-10-30 14:09:58 +0530

    optimal using binary search on answers

--- binary search/Binary search on answers/5smallest_divisor.cpp
+++ binary search/Binary search on answers/5smallest_divisor.cpp
@@ -21,8 +21,41 @@ int brute(vector<int>& nums, int threshold) {
     return -1;
 }
 
+int compute_sum(vector<int> nums, int divisor){
+    int sum = 0;
+    for(int i = 0;i<nums.size();i++){
+        sum +=ceil((double)nums[i]/divisor);
+    }
+    return sum;
+}
+
+int smallest_divisor_optimal(vector<int> nums, int threshold){
+    int low = 1;
+    int high = findMaxInVec(nums);
+    int ans = -1;
+
+    while(high>=low){
+        int mid = (low+high)/2;
+        
+        int sum = compute_sum(nums,mid);
+
+        if(sum<=threshold){
+            // mid can be possible ans but we need to find minimum
+            ans = mid;
+            // eliminate right
+            high = mid - 1;
+        }else{
+            // eliminate left
+            low = mid + 1;
+        }
+    }
+    return ans;
+    // return low; to save space
+}
+
 int main(){
     vector<int> nums = {1,2,5,9};
-    cout << brute(nums,6);
+    // cout << brute(nums,6);
+    cout << smallest_divisor_optimal(nums,6);
     return 0;
 }
