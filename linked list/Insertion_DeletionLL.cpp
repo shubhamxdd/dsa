d5de9e84f286cb9fc84d93caf3903b578430c22f
shubhamxdd <ssisodia128@gmail.com>
2025-11-05 15:31:31 +0530

    delete from head of LL delete from tail of LL delete kth element in LL delete element with value x in LL

---
+++ linked list/Insertion_DeletionLL.cpp
@@ -0,0 +1,170 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node(int data1, Node*next1){
+            data=data1;
+            next = next1;
+        }
+        Node(int data1){
+            data = data1;
+            next = nullptr;
+        }
+};
+
+// array to LinkedList
+Node* arrayTOLL(vector<int> &arr){
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+
+    for (int i = 1; i < arr.size(); i++){
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = mover->next;
+    }
+    return head;
+
+}
+
+// traverse linkedList
+void traverseLL(Node *headLL){
+    Node* temp = headLL;
+    while (temp){
+        cout << temp->data << " ";
+        temp=temp->next;
+    }
+    cout << endl;
+}
+
+// delete from linkedList
+Node* deleteHeadFromLL(Node* head){
+    if(head == NULL) return head;
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+    return head;
+}
+
+Node* deleteTailFromLL(Node* head){
+    if(head == NULL || head->next == NULL) return NULL;
+    Node *temp = head;
+    while (temp->next->next !=NULL){
+        temp = temp->next;
+    }
+    // got second last element
+    temp->next = NULL;
+    free(temp->next);
+    return head;
+}
+
+
+/*
+// k =2or 3 in this case
+Node* deleteFromMidLL(Node* head,int k){
+    if(head == NULL) return head; 
+    Node* temp = head;
+    Node* prev = NULL; // store prev of temp
+    int count = 0;
+
+    while(temp != NULL){
+        count++;
+        if(count == k){
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+
+    // this prev will point to node we have to delete
+    for(int i = 1;i<k;i++){
+        prev = prev->next;
+    }
+    // temp pointer points to a node before prev
+    for(int i = 1;i<k-1;i++){
+        temp = temp->next;
+    }
+    // store next of next element in next of temp
+    temp->next = temp->next->next;
+    free(prev);
+    return head;
+}
+*/   
+
+Node* deleteKthElement(Node* head, int k){
+    if(head == NULL) return head;
+    Node *temp = head;
+    Node *prev = NULL;
+    int count = 0;
+    
+    if(k==1){
+        head=head->next;
+        delete temp;
+        return head;
+    }
+
+    while(temp!=NULL){
+        count++;
+        if(count == k){
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        prev=temp;
+        temp=temp->next;
+    }
+    return head;
+}
+
+
+Node* deleteWithValue(Node* head, int val){
+    if(head == NULL) return head;
+    Node *temp = head;
+    Node *prev = NULL;
+    // int currVal = head->data;
+
+    if(head->data == val) {
+        head=head->next;
+        delete temp;
+        return head;
+    };
+
+    while(temp!=NULL){
+        if(temp->data == val){
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        prev=temp;
+        temp=temp->next;
+        // currVal = temp->data;
+    }
+    return head;
+}
+
+
+
+
+int main(){
+    vector<int> arr = {4,5,3,2};
+    Node * headOFLL = arrayTOLL(arr);
+    traverseLL(headOFLL);
+    // Node* newHead = deleteHeadFromLL(headOFLL);
+    // Node *newHead = deleteTailFromLL(headOFLL);
+
+    /*
+    Node *newHead = deleteKthElement(headOFLL,1);
+    traverseLL(newHead);
+    */
+
+    Node *newHead = deleteWithValue(headOFLL,4);
+    traverseLL(newHead);
+
+    // cout << headOFLL->data;
+    return 0;
+}
