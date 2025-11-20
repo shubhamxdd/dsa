47810ac6bd3c3bb00d91b64643bf225a442cdd51
shubhamxdd <ssisodia128@gmail.com>
2025-11-21 00:21:18 +0530

    reverse LL brute (using stack)

---
+++ linked list/206Reverse_LL.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *next;
+    
+    Node(int dataa, Node* nextt){
+        data = dataa;
+        next = nextt;
+    }
+    Node(int dataa){
+        data = dataa;
+        next = nullptr;
+    }
+};
+
+Node *arrayToLL(vector<int> arr){
+    Node *head = new Node(arr[0]);
+
+    Node *temp = head;
+
+    for(int i = 1;i<arr.size();i++){
+        Node *newNode = new Node(arr[i]);
+        temp->next = newNode;
+        temp=temp->next;
+    }
+    return head;
+}
+
+void traverseLL(Node *head){
+    Node *temp = head;
+
+    while(temp!=nullptr){
+        cout << temp->data << " ";
+        temp=temp->next;
+    }
+    cout << endl;
+}
+
+Node *reverseLLStack(Node *head){
+    stack<int> st;
+
+    Node *temp = head;
+
+    while(temp){
+        st.push(temp->data);
+        temp=temp->next;
+    }
+
+    temp = head;
+
+    while(temp!=nullptr){
+        temp->data = st.top();
+        st.pop();
+        temp=temp->next;
+    }
+    return head;
+    
+}
+
+int main(){
+    vector<int> arr = {1,3,2,5};
+    Node *head = arrayToLL(arr);
+    traverseLL(head);
+    Node *newHead = reverseLLStack(head);
+    traverseLL(newHead);
+    // traverseLL(head);
+
+    return 0;
+}
