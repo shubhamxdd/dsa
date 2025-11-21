7b1ab71553011845caae2b49dc4d4b9eed79e6f8
shubhamxdd <ssisodia128@gmail.com>
2025-11-21 23:20:14 +0530

    optimal (TODO)

--- linked list/324Palindrome_LL.cpp
+++ linked list/324Palindrome_LL.cpp
@@ -58,11 +58,55 @@ bool validPalindrome(Node *head){
 }
 
 
+Node *reverseLLOptimal(Node *head){
+    Node *temp = head;
+    Node *prev = nullptr;
+
+    while(temp!=nullptr){
+        Node *front = temp->next;
+        temp->next = prev;
+        prev=temp;
+        temp=front;
+    }
+    return prev;
+}
+
+bool validPalindromeOptimal(Node *head){
+    Node *slow = head;
+    Node* fast = head;
+
+    // 1. identify middle
+    while(fast->next!=nullptr && fast->next->next!=nullptr){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    // 2. reverse second half
+    Node *newHead = reverseLLOptimal(slow->next);
+
+    // 3. check for palindrome
+
+    Node *first = head;
+    Node *second = newHead;
+
+    while(second!=nullptr){
+        if(first->data != second->data) {
+            reverseLLOptimal(newHead);
+            return false;
+        }
+        first = first->next;
+        second = second->next;
+    }
+    reverseLLOptimal(newHead);
+    return true;
+}
+
 int main(){
     // vector<int> arr = {4,1,2,3};
     vector<int> arr = {1,2,3,2,1};
     Node *head = convertArrtoLL(arr);
     traverseLL(head);
-    cout << validPalindrome(head) << endl;
+    // cout << validPalindrome(head) << endl;
+    cout << validPalindromeOptimal(head) << endl;
     return 0;
 }
