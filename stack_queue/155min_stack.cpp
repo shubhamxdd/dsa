2686b984e5436c46c994064762285749d155eac6
shubhamxdd <ssisodia128@gmail.com>
2025-12-05 14:45:23 +0530

    optimal

--- stack_queue/155min_stack.cpp
+++ stack_queue/155min_stack.cpp
@@ -2,27 +2,56 @@
 using namespace std;
 
 class MinStack {
-    stack<pair<int, int>> st; // {current elem, current_min}
+public:
+    stack<long long> st; // {current elem, current_min}
+    long long minV = INT_MAX;
 
-    public:
-        void push(int x){
-            if(st.empty()){
-                st.push({x,x});
-            }else {
-                // int min = st.top().second;
-                st.push({x, min(x, st.top().second)});
+    MinStack() { minV = LLONG_MAX; }
+
+    void push(long long x) {
+        if (st.empty()) {
+            minV = x;
+            st.push(x);
+        } else {
+            if (x >= minV) { // ✅ Also fix: use >= not >
+                st.push(x);
+            } else {
+                long long oldMin = minV;   // ✅ Save old minimum
+                minV = x;                  // Update to new minimum
+                st.push(2LL * x - oldMin); // ✅ Use old minimum in formula
             }
         }
-        void pop(){
-            st.pop();
+    }
+    void pop() {
+        if (st.empty())
+            return;
+
+        long long x = st.top();
+        st.pop();
+        if (x < minV) {
+            minV = 2 * minV - x;
         }
-        int top(){
-            return st.top().first;
-        }
-        int getMin(){
-            return st.top().second;
-        }
+    }
+    long long top() {
+        if (st.empty())
+            return -1;
+
+        long long x = st.top();
+        if (x > minV)
+            return x;
+        return minV;
+    }
+    int getMin() { return minV; }
 };
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
 
 int main(){
     return 0;
