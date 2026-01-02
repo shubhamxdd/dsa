36c1026e9dea05aafa3661858f88347fdacd25ac
shubhamxdd <ssisodia128@gmail.com>
2026-01-02 21:37:54 +0530

    optimal(2ptr)

--- array_ques/medium/11Container_With_Most_Water.cpp
+++ array_ques/medium/11Container_With_Most_Water.cpp
@@ -18,8 +18,35 @@ int brute(vector<int> &arr){
     return maxArea;
 }
 
+int optimal(vector<int> &arr){
+    int n = arr.size();
+    int left = 0;
+    int right = n-1;
+
+    int maxArea = 0;
+
+    while(right>left){
+        int height = min(arr[left],arr[right]);
+        int width = right-left;
+        int area = height*width;
+
+        maxArea = max(area,maxArea);
+
+        // trim down search space
+        // remove smaller element 
+
+        if(arr[left]>arr[right]){
+            right--;
+        }else{
+            left++;
+        }
+    }
+    return maxArea;
+}
+
 int main(){
     vector<int> arr = {1,8,6,2,5,4,8,3,7};
-    cout<<brute(arr);
+    // cout<<brute(arr);
+    cout<<optimal(arr);
     return 0;
 }
