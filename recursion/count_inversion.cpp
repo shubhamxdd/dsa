0e0c0f699c33bf40e17284d683de2f0ff9225e6d
shubhamxdd <ssisodia128@gmail.com>
2026-01-14 11:13:51 +0530

    optimal using merge sort

--- recursion/count_inversion.cpp
+++ recursion/count_inversion.cpp
@@ -17,11 +17,57 @@ int countInversionsBrute(vector<int> &arr){
 
 }
 
+int merge(vector<int> &arr,int start, int mid, int end){
+    vector<int> temp;
+    int i = start;
+    int j= mid+1;
 
+    int invCnt = 0;
 
+    while(i<=mid && j<=end){
+        if(arr[i]<=arr[j]){
+            temp.push_back(arr[i]);
+            i++;
+        }else{
+            // means a[i]>a[j], inversion found!
+            temp.push_back(arr[j]);
+            invCnt+=mid-i+1;
+            j++;
+        }
+    }
+    while(i<=mid){
+        temp.push_back(arr[i]);
+        i++;
+    }
+    while(j<=end){
+        temp.push_back(arr[j]);
+        j++;
+    }
+    //copy values in original array
+    for(int idx = 0;idx<temp.size();idx++){
+        arr[idx+start] = temp[idx];
+    }
+    return invCnt;
+}
+
+int mergeSort(vector<int> &arr, int start, int end){
+    if(start<end){
+        int mid = (start+end)/2;
+
+        // left half
+        int left = mergeSort(arr,start,mid);
+        // right half
+        int right = mergeSort(arr,mid+1,end);
+
+        int invCnt = merge(arr,start,mid,end);
+        return left+right+invCnt;
+    }
+    return 0;
+}
 
 int main(){
     vector<int> arr = {6,3,5,2,7};
-    cout << countInversionsBrute(arr);
+    // cout << countInversionsBrute(arr);
+    cout << mergeSort(arr,0,arr.size()-1);
     return 0;
 }
