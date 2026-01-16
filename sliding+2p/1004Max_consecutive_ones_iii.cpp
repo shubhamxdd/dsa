153386df7752cfe347f2d38440f403b6fb356628
shubhamxdd <ssisodia128@gmail.com>
2026-01-16 12:40:58 +0530

    better

--- sliding+2p/1004Max_consecutive_ones_iii.cpp
+++ sliding+2p/1004Max_consecutive_ones_iii.cpp
@@ -18,10 +18,31 @@ int maxConseqOnesBrute(vector<int> &arr,int k){
     return maxLen;
 }
 
+int maxConseqOnesBetter(vector<int> &nums, int k){
+    int l = 0;
+    int r = 0;
+    int zeros = 0;
+    int maxLen = 0;
+    int n = nums.size();
 
+    while(r<n){
+        if(nums[r] == 0) zeros++;
+        while(zeros > k) {
+            if(nums[l] == 0) zeros--;
+            l++;
+        }
+        if(zeros<=k){
+            int len = r-l+1;
+            maxLen = max(maxLen,len);
+        }
+        r++;
+    }
+    return maxLen;
+}
 
 int main(){
     vector<int> arr = {1,1,1,0,0,0,1,1,1,1,0};
-    cout << maxConseqOnesBrute(arr,2);
+    // cout << maxConseqOnesBrute(arr,2);
+    cout << maxConseqOnesBetter(arr,2);
     return 0;
 }
