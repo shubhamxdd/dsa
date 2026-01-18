f89809c548d30fa82cea4c696027236b4084be7a
shubhamxdd <ssisodia128@gmail.com>
2026-01-18 17:40:48 +0530

    optimal

--- sliding+2p/1248Count_Number_of_Nice_Subarrays.cpp
+++ sliding+2p/1248Count_Number_of_Nice_Subarrays.cpp
@@ -15,8 +15,38 @@ int numberOfSubarraysBrute(vector<int>& nums, int k) {
     return count;
 }
 
+
+// same as binary subarray optimal
+int calculate(vector<int>& nums, int k) {
+    if(k<0) return 0;
+    int n = nums.size();
+    int l = 0;
+    int r = 0;
+    int oddNums = 0;
+    int count = 0;
+
+    while(r<n){
+        oddNums+=nums[r] % 2;
+        while(oddNums>k){
+            oddNums -= nums[l] % 2;
+            l++;
+        }
+        count += (r-l+1);
+        r++;
+    }
+    return count;
+}
+
+
+int numberOfSubarraysOptimal(vector<int> &arr, int k){
+    return calculate(arr,k) - calculate(arr,k-1);
+}
+
+
+
 int main(){
     vector<int> arr = {1,1,2,1,1};
-    cout << numberOfSubarraysBrute(arr,3);
+    // cout << numberOfSubarraysBrute(arr,3);
+    cout << numberOfSubarraysOptimal(arr,3);
     return 0;
 }
