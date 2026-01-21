3fba41e0c2399c68a52577701f3c8717b5faf753
shubhamxdd <ssisodia128@gmail.com>
2026-01-21 15:56:02 +0530

    optimal 4sum

--- 4s.cpp
+++ 4s.cpp
@@ -50,12 +50,46 @@ vector<vector<int>> fourSumBetter(vector<int>& nums, int target) {
     return ans;
 }
 
+vector<vector<int>> fourSumOptimal(vector<int>& nums, int target) {
+    sort(nums.begin(),nums.end());
+    int n = nums.size();
+    vector<vector<int>> ans;
+    for(int i = 0;i<n;i++){
+        // should not be same as previous element
+        if(i>0 && nums[i] == nums[i-1]) continue;
+        for(int j = i+1;j<n;j++){
+            if(j>i+1 && nums[j] == nums[j-1]) continue;
+            int k = j+1;
+            int l = n-1;
 
+            while(k<l){
+                long long sum = (long long)nums[i] + (long long)nums[j] + (long long)nums[k] + (long long)nums[l];
+                if(sum>target)l--;
+                else if(sum<target)k++;
+                else {
+                    // both are equal
+                    // quad found
+                    vector<int> quad = {nums[i],nums[j],nums[k],nums[l]};
+                    // no need to sort as we sorted the array in first step
+                    ans.push_back(quad);
+                    k++;
+                    l--;
+                    // now move k and l pointer to a new value which is not same as previous
+                    while(k<l && nums[k]==nums[k-1]) k++;
+                    while(k<l && nums[l]==nums[l+1]) l--;
+                }
+            }
+
+        }
+    }
+    return ans;
+}
 
 int main(){
     vector<int> arr = {1,0,-1,0,-2,2};
     // vector<vector<int>> result = fourSumBrute(arr,0);
-    vector<vector<int>> result = fourSumBetter(arr,0);
+    // vector<vector<int>> result = fourSumBetter(arr,0);
+    vector<vector<int>> result = fourSumOptimal(arr,0);
     for(auto vec : result){
         for(auto val : vec){
             cout<<val<<" ";
