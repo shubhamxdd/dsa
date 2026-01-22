10c164577f7b6b1c8832e57b69ab9a846e26dc25
shubhamxdd <ssisodia128@gmail.com>
2026-01-22 14:09:14 +0530

    preorder tree traversal

---
+++ trees/main.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+        Node(int val){
+            data = val;
+            left = nullptr;
+            right = nullptr;
+        }
+};
+
+
+void preorder(Node *node){
+    if(node == nullptr) return;
+    cout << node->data << " ";
+    preorder(node->left);
+    preorder(node->right);
+}
+
+int main(){
+    // Creating nodes
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    // Simple output to verify tree structure
+    cout << "Root Node: " << root->data << endl;
+    cout << "Left Child of Root: " << root->left->data << endl; 
+    cout << "Right Child of Root: " << root->right->data << endl;
+    cout << "Left Child of Left Node: " << root->left->left->data << endl;
+    cout << "Right Child of Left Node: " << root->left->right->data << endl;
+
+
+
+    // preorder traversal
+    preorder(root);
+
+
+    return 0;
+}
