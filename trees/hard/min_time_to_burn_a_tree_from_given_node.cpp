3384b00e9088f71f822259c051bdec488e104e96
shubhamxdd <ssisodia128@gmail.com>
2026-02-08 15:41:12 +0530

    another of similar pattern

---
+++ trees/hard/min_time_to_burn_a_tree_from_given_node.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+        Node(int v){
+            val = v;
+            left= nullptr;
+            right= nullptr;
+        }
+};
+
+void getParentNodes(Node* root, unordered_map<Node*,Node*> &track_parents){
+    if(!root) return;
+    queue<Node*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        Node* curr = q.front();
+        q.pop();
+
+        if(curr->left){
+            track_parents[curr->left] = curr;
+            q.push(curr->left);
+        }
+        if(curr->right){
+            track_parents[curr->right] = curr;
+            q.push(curr->right);
+        }
+    }
+}
+
+int minTimeToBurnTree(Node* root, Node* target){
+    if(!root || !target) return 0;
+    unordered_map<Node*,Node*> track_parents;
+    getParentNodes(root,track_parents);
+
+    unordered_map<Node*,bool> visited;
+
+    queue<Node*> q;
+    q.push(target);
+    visited[target] = true;
+
+    int curr_time = 0;
+
+    while(!q.empty()){
+        int size = q.size();
+        bool burned = false;
+        // curr_time++;
+        for(int i = 0;i<size;i++){
+            Node* curr = q.front();
+            q.pop();
+
+            // traverse it radially, top, left and right
+            if(curr->left && !visited[curr->left]){
+                // push in q and mark as visited
+                q.push(curr->left);
+                visited[curr->left] = true;
+                burned = true;
+            }
+            if(curr->right && !visited[curr->right]){
+                // push in q and mark as visited
+                q.push(curr->right);
+                visited[curr->right] = true;
+                burned = true;
+            }
+            // parent node
+            if(track_parents[curr] && !visited[track_parents[curr]]){
+                // push in q and mark as visited
+                q.push(track_parents[curr]);
+                visited[track_parents[curr]] = true;
+                burned = true;
+            }
+            if(burned == true) curr_time++;
+        }
+
+    }
+
+    return curr_time;
+}
+
+int main(){
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right = new Node(3);
+
+    cout << minTimeToBurnTree(root,root->left);
+
+
+    return 0;
+}
